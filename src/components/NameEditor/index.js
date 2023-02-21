11c3956765913bf39a4b4dac7b151441ec4aa4e2
bbeatrice <techerbaretbeatrice@gmail.com>
2023-02-21 10:21:45 +0100

    edit name style

--- src/components/NameEditor/index.js
+++ src/components/NameEditor/index.js
@@ -19,15 +19,17 @@ const NameEditor = () => {
         {!user.openEditor && <button className={`edit-button ${``}`} onClick={() => store.dispatch(userSlice.actions.displayEditor())}>
             Edit Name
         </button>}
-        {user.openEditor && <form onSubmit={submit}>
-            <div>
+        {user.openEditor && <form className='name-editor' onSubmit={submit}>
+            <div className="input-container">
                 <label></label>
-                <input name="firstName" defaultValue={user.firstName} />
+                <input className="input-field first-name" name="firstName" defaultValue={user.firstName} />
                 <label></label>
-                <input name="lastName" defaultValue={user.lastName} />
+                <input className="input-field last-name" name="lastName" defaultValue={user.lastName} />
             </div>
-            <button type="submit">Save</button>
-            <button type="cancel" onClick={() => store.dispatch(userSlice.actions.hideEditor())}>cancel</button>
+            <div className="button-container">
+                <button className="edit-button save" type="submit">Save</button>
+                <button className="edit-button cancel" type="cancel" onClick={() => store.dispatch(userSlice.actions.hideEditor())}>cancel</button>
+            </div>
         </form>
 
         }
